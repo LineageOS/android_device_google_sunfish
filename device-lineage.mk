de99b0fcfd683e743d5432720fe1f0319917d7e3
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    sunfish: Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- device-lineage.mk
+++ device-lineage.mk
@@ -45,6 +45,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 
