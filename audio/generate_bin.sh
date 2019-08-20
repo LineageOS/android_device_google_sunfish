cffd10222fa1d52f95677c9f20e5fcb24c3b003d
Wilson Sung <wilsonsung@google.com>
2019-08-21 00:27:20 +0800

    Add audio config fix audioserver crash
    Bug: 141230286
    Change-Id: I182e29fb0d6b22858932c8c7cc26ca787a32464d
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

---
+++ audio/generate_bin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# B1
+python3.5 convert_config_string.py txt 20180509_100_Sargo_Stereo_Protect.h crus_sp_config_b1_rx.bin crus_sp_config_b1_tx.bin
+
+# C1
+python3.5 convert_config_string.py txt 20180509_Bonito_Protect_24bit.h crus_sp_config_c1_rx.bin crus_sp_config_c1_tx.bin
