897488cc099ef88c8b6539e315d10abce0437162
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    sunfish: Build a disabled vbmeta image
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -23,3 +23,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := sunfish_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/sunfish
 TARGET_NEEDS_DTBOIMAGE := true
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
