7759c95b548f3ae990f64844975ee725bd8faa7e
Cyan_Hsieh <cyanhsieh@google.com>
2019-10-01 13:19:00 +0800

    Disable android.hardware.camera.provider@2.4
    Bug: 140395581
    Change-Id: I4d91c75aaffa64cb9b6fa0c0abd21fe52923d01a

--- device.mk
+++ device.mk
@@ -373,8 +373,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.media.c2@1.0-service \
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
     camera.sm6150 \
     libgooglecamerahal \
     libgooglecamerahwl_impl \
