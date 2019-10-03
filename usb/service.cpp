e2e286e15ba591209169ab17538f6fa0a58db1a4
Cyan_Hsieh <cyanhsieh@google.com>
2019-10-03 15:43:34 +0800

    Revert "Modify android.hardware.usb to 1.1"
    This reverts commit e50629f51b68c5daa89ac465347fe39f9fcc047a.
    
    Test: adb work
    Change-Id: I6df9c37bb118edce5876762e324b68f64b672fdb

--- usb/service.cpp
+++ usb/service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.usb@1.1-service.sunfish"
+#define LOG_TAG "android.hardware.usb@1.2-service.sunfish"
 
 #include <hidl/HidlTransportSupport.h>
 #include "Usb.h"
@@ -27,10 +27,10 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::usb::V1_1::IUsb;
 using android::hardware::usb::gadget::V1_0::IUsbGadget;
-using android::hardware::usb::V1_1::implementation::Usb;
 using android::hardware::usb::gadget::V1_0::implementation::UsbGadget;
+using android::hardware::usb::V1_2::IUsb;
+using android::hardware::usb::V1_2::implementation::Usb;
 
 using android::OK;
 using android::status_t;
@@ -59,5 +59,4 @@ int main() {
     // Under noraml cases, execution will not reach this line.
     ALOGI("USB HAL failed to join thread pool.");
     return 1;
-
 }
