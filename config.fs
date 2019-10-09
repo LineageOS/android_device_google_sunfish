6b0cbed1e177f7117037d22dec380d6c0704565c
chenpaul <chenpaul@google.com>
2019-10-09 21:36:40 +0800

    wifi - change owner/group for cnss-daemon
    Bug: 142299454
    Test: Check owner/group for cnss-daemon
    Change-Id: Ie6b58a1c4f2a55f9cbc21a4604f37b6479f24397

--- config.fs
+++ config.fs
@@ -34,8 +34,8 @@ value:2906
 
 [vendor/bin/cnss-daemon]
 mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
