0fca23f2942747016acb3b9277190026594861a3
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    sunfish: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_sunfish.mk \
     $(LOCAL_DIR)/aosp_sunfish_hwasan.mk \
+    $(LOCAL_DIR)/lineage_sunfish.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_sunfish-userdebug \
+    lineage_sunfish-userdebug
