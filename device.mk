017558a278cca3119655958ec977ea978d8f2949
Shinru Han <shinruhan@google.com>
2019-12-13 21:00:36 +0800

    gps: Add geofence & batching libs
    Test: geofence & batching interface can be loaded
    Bug: 146191305
    Change-Id: I427ed4a9294ba69a87f42ee7888278906b7de4ea

--- device.mk
+++ device.mk
@@ -486,7 +486,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libgps.utils \
     libgnss \
+    libloc_core \
     liblocation_api \
+    libbatching \
+    libgeofencing \
     android.hardware.gnss@2.0-impl-qti \
     android.hardware.gnss@2.0-service-qti
 
