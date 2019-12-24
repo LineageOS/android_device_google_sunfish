66ac9c31f6ca5c78c684f4a4d4e2bd8af06c21f5
Adam Shih <adamshih@google.com>
2019-12-24 14:07:03 +0800

    put un-solved denails in a different folder
    Bug: 122999081
    Test: boot to home with no avc error appeared
    Change-Id: I480ef7ba398a52d9eb8494d3a4a64e1ac16441e6

--- sunfish-sepolicy.mk
+++ sunfish-sepolicy.mk
@@ -5,4 +5,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/sunfish-sepolicy/private
 BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/google
 BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/qcom/common
 BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/qcom/sm7150
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/tracking_denials
 
