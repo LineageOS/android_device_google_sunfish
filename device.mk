4a0f8ce17be48a7a176482829d5de38f7e0735b4
Hongbo Zeng <hongbozeng@google.com>
2020-02-11 01:24:47 +0800

    [Single RIL w/ SM7250] Add feature flag and related soong namespaces on SM7150
    - Add DEVICE_USES_SM7250_QCRIL_TELEPHONY to enable/disable Single RIL feature
    - Add soong namespaces for:
      vendor/qcom/sm7150/codeaurora/telephony/ims
      vendor/qcom/sm7150/proprietary/data/permissions
      vendor/qcom/sm7150/proprietary/qcril-data-hal
      vendor/qcom/sm7150/proprietary/qcril-hal
    
    Bug: 146591878
    Test: sanity(VoLTE/WFC, SMS/MMS, browsing) pass.
    Change-Id: Ief6cb2598efa92f714755b370ee8867421cde790

--- device.mk
+++ device.mk
@@ -31,19 +31,17 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/sm7150 \
     vendor/google/interfaces
 
-# Single vendor RIL/Telephony/data with SM8150
-DEVICE_USES_SM8150_QCRIL_TELEPHONY := true
+# Single vendor RIL/Telephony/data with SM7250
+DEVICE_USES_SM7250_QCRIL_TELEPHONY := true
 
-ifeq ($(DEVICE_USES_SM8150_QCRIL_TELEPHONY), true)
+ifeq ($(DEVICE_USES_SM7250_QCRIL_TELEPHONY), true)
   PRODUCT_SOONG_NAMESPACES += \
-      vendor/qcom/sm8150/codeaurora/telephony/ims \
-      vendor/qcom/sm8150/proprietary/data/permissions \
-      vendor/qcom/sm8150/proprietary/qcril-data-hal/qdp \
-      vendor/qcom/sm8150/proprietary/qcril-data-hal/util \
-      vendor/qcom/sm8150/proprietary/qcril-data-hal/datamodule \
-      vendor/qcom/sm8150/proprietary/qcril-hal
+      vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims \
+      vendor/qcom/sm7250/proprietary/qcril-data-hal \
+      vendor/qcom/sm7250/proprietary/qcril-hal \
+      vendor/qcom/sm8150/proprietary/data/permissions
 else
-  $(warning DEVICE_USES_SM8150_QCRIL_TELEPHONY is disabled)
+  $(warning DEVICE_USES_SM7250_QCRIL_TELEPHONY is disabled)
 
   PRODUCT_SOONG_NAMESPACES += \
       vendor/qcom/sm7150/codeaurora/commonsys/telephony/ims \
