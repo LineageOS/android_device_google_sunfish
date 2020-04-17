76d9639ac5910e853e4355b432221f586ea6be93
Chirayu Desai <chirayudesai1@gmail.com>
2020-04-17 19:30:31 +0530

    sunfish: Inline kernel building
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Change-Id: I1cc3ab1ff45a18d686107a7495b80777b769820f

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+KERNEL_LD := LD=ld.lld
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/tools-lineage/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := sunfish_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
+TARGET_NEEDS_DTBOIMAGE := true
