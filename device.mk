a2a62197982630bee28a6de0e6e71d477349e5d9
YH_Lin <yhli@google.com>
2020-05-06 04:14:43 +0800

    sunfish: use the IBootControl 1.1 HAL
    Bug: 146467096
    Test: build pass and test storage-qa
    
    Signed-off-by: YH_Lin <yhli@google.com>
    Change-Id: I5b9b1ffe4bf8dc12117e95635fd55871996e078f

--- device.mk
+++ device.mk
@@ -497,9 +497,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-impl-pixel-legacy \
+    android.hardware.boot@1.1-impl-pixel-legacy.recovery \
+    android.hardware.boot@1.1-service \
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
