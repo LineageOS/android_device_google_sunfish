5e648a4ebb240ee0b73abec0b5946d70c69771fe
Kenny Root <kroot@google.com>
2020-06-09 13:59:23 -0700

    Citadel: move rules to common directory
    Move all the common Citadel rules to a directory where they can all be
    changed simultaneously and avoid accidental version skew between the
    devices.
    
    (cherry picked from commit 7fbb5e3eef835d482b6cf37037ecb82eb03bbd3c)
    
    Test: build affected devices locally
    Bug: 143330574
    Merged-In: I3aa9355f99ff7842925e30d448ce3b0d335491c5
    Change-Id: I3aa9355f99ff7842925e30d448ce3b0d335491c5

--- sunfish-sepolicy.mk
+++ sunfish-sepolicy.mk
@@ -10,4 +10,5 @@ BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/st
 BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/verizon
 
 # Pixel-wide sepolicy
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/citadel
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
