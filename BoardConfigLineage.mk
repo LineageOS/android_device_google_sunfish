738a5294c38fc5e2ce5afe2ed551d15d418aaa1e
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    sunfish: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
     * Our panel does get along with Adaptive Backlight
       features, so leave IAdaptiveBacklight in place.
     * AOSP declares `ro.vendor.display.foss` but we don't
       have support for IAdaptiveBacklight, so don't declare.
    
    Change-Id: Ia96134e0a4bb06269b48671a87991d5e7a53d9ed

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,6 +13,10 @@ TARGET_KERNEL_CONFIG := sunfish_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Manifests
+DEVICE_MANIFEST_FILE += device/google/sunfish/lineage_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
@@ -27,6 +31,10 @@ BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1359413248
 BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT := -1
 BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
 endif
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
