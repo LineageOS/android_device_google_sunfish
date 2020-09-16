8f9a4b51e8173b0ca135198a9aabb4aebb8a0118
Paul Scovanner <pscovanner@google.com>
2020-09-16 16:56:15 +0000

    Update Sunfish SVN to 15
    Bug: 168709737
    Change-Id: Ia0679bc0f4c6668a01d3dc1485d062ce7bcff360
    (cherry picked from commit 60869c86b8c1eafed70ce9ff69fc35b6c7d63a9f)

--- device.mk
+++ device.mk
@@ -878,7 +878,7 @@ PRODUCT_PACKAGES += $(HIDL_WRAPPER)
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.build.svn=14
+	ro.vendor.build.svn=15
 
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
