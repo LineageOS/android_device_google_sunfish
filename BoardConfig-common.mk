ec56cf292c559ca72959705a1ef213317e8672bd
Justin Yun <justinyun@google.com>
2020-09-25 17:30:22 +0900

    Move device specific FCM to product partition
    Install device specific compatibility matrix to product partition.
    As the manifest for the atfwdcmd is included as a fragment, remove
    it from the framework_manifest.xml, that is empty and removed.
    
    Bug: 151690176
    Bug: 130714844
    Test: make and boot that checks VINTF
    Change-Id: I22640a0c48454e1cfd7b7da11901b64dbde158c4

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -202,8 +202,10 @@ TARGET_USES_DRM_PP := true
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := device/google/sunfish/manifest.xml
 DEVICE_MATRIX_FILE := device/google/sunfish/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/sunfish/device_framework_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := device/google/sunfish/framework_manifest.xml
+# Install product specific framework compatibility matrix
+# (TODO: b/169535506) This includes the FCM for system_ext and product partition.
+# It must be split into the FCM of each partition.
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := device/google/sunfish/device_framework_matrix_product.xml
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
