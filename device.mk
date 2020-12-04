0ad1a7f310566b6e8bf8f76680c81cb3c330b4a6
Wei Wang <wvw@google.com>
2020-12-04 03:10:12 +0000

    Revert^2 "Add cpu controller into task_profiles.json"
    11b51a1eb2bc6efc3c08a5f80342b6d4cd3bfe5d
    
    Change-Id: I6c27e3cb78e88180e878ae2a0110101ec776b0d8

--- device.mk
+++ device.mk
@@ -863,6 +863,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Vendor verbose logging default property
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PROPERTY_OVERRIDES += \
