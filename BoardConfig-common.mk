425212edfa394b140c626253e4bc646e573b3952
Tianjie <xunchang@google.com>
2020-12-10 17:22:08 -0800

    Include the Pixel common board configuration
    Bug: 156162446
    Test: treehugger
    Change-Id: Iec908f4cef5d28c2f621e6d8bc845b9c0cb26761

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -15,6 +15,7 @@
 #
 
 include build/make/target/board/BoardConfigMainlineCommon.mk
+include build/make/target/board/BoardConfigPixelCommon.mk
 
 TARGET_BOARD_PLATFORM := sm6150
 TARGET_BOARD_INFO_FILE := device/google/sunfish/board-info.txt
