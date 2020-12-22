6d9e737aecb76b29ab05ec7a6f15ee3545afb03f
Joe Maples <joe@frap129.org>
2020-12-22 17:57:33 +0800

    sunfish: Disable subsytem ramdumps on userdebug
    Change-Id: Ibb0deec7e9ccac40d86e5e4eeaef485f33fa71eb

--- device.mk
+++ device.mk
@@ -650,7 +650,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.snd_card.open.retries=50
 
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 # Subsystem ramdump
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sys.ssr.enable_ramdumps=1
