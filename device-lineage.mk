15e272713e80b28ba90a95e668191a13d2662093
Peter Cai <peter@typeblog.net>
2020-12-23 17:09:47 +0800

    sunfish: Set up proprietary files extraction & build vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I17f79b417a8f0d022bec9c6b5f37d03b842d43e1

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,3 +15,133 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
+
+# Build necessary packages for system_ext
+
+# Display
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
+
+# Build necessary packages for vendor
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
+    hardware.google.bluetooth.sar@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+
+# CHRE
+PRODUCT_PACKAGES += \
+    chre
+
+# Codec2
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor \
+    libmedia_ecoservice.vendor \
+    libstagefright_bufferpool@2.0.1.vendor
+
+# Confirmation UI
+PRODUCT_PACKAGES += \
+    android.hardware.confirmationui@1.0.vendor:64 \
+    libteeui_hal_support.vendor:64
+
+# Display
+PRODUCT_PACKAGES += \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
+
+# HIDL
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
+
+# Json
+PRODUCT_PACKAGES += \
+    libjson \
+
+# Nos
+PRODUCT_PACKAGES += \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat
+
+# Sensor listener
+PRODUCT_PACKAGES += \
+    lib_sensor_listener
+
+# Tinycompress
+PRODUCT_PACKAGES += \
+    libtinycompress
+
+# VNDK FWK detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwifi-hal:64 \
+    libwifi-hal-qcom
+
+# Misc interfaces
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.stats@1.0.vendor:64 \
+    android.hardware.authsecret@1.0.vendor \
+    android.hardware.biometrics.fingerprint@2.1.vendor:64 \
+    android.hardware.biometrics.fingerprint@2.2.vendor:64 \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.input.classifier@1.0.vendor:64 \
+    android.hardware.input.common@1.0.vendor:64 \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.neuralnetworks@1.0.vendor:64 \
+    android.hardware.neuralnetworks@1.1.vendor:64 \
+    android.hardware.neuralnetworks@1.2.vendor:64 \
+    android.hardware.neuralnetworks@1.3.vendor:64 \
+    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.power-V1-ndk_platform.vendor \
+    android.hardware.radio.config@1.0.vendor:64 \
+    android.hardware.radio.config@1.1.vendor:64 \
+    android.hardware.radio.config@1.2.vendor:64 \
+    android.hardware.radio.deprecated@1.0.vendor:64 \
+    android.hardware.radio@1.2.vendor:64 \
+    android.hardware.radio@1.3.vendor:64 \
+    android.hardware.radio@1.4.vendor:64 \
+    android.hardware.radio@1.5.vendor:64 \
+    android.hardware.secure_element@1.1.vendor:64 \
+    android.hardware.secure_element@1.2.vendor:64 \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.sensors@2.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.hardware.weaver@1.0.vendor:64 \
+    android.hardware.wifi@1.1.vendor:64 \
+    android.hardware.wifi@1.2.vendor:64 \
+    android.hardware.wifi@1.3.vendor:64 \
+    android.hardware.wifi@1.4.vendor:64 \
+    android.hardware.wifi@1.5.vendor:64 \
+    android.system.net.netd@1.1.vendor:64
