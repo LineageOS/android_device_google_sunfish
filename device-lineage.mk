282bae0aa07d53f4dc4e2490bd675e0b94730a59
Peter Cai <peter@typeblog.net>
2020-12-23 20:44:38 +0800

    sunfish: proprietary-files: Build OSS components when possible
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Change-Id: I575f2f3f061850ea41970385449cadb97087fb53

--- device-lineage.mk
+++ device-lineage.mk
@@ -24,15 +24,59 @@ PRODUCT_COPY_FILES += \
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 
+# Build necessary packages for system_ext
+PRODUCT_PACKAGES += \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    qti-telephony-utils \
+    vendor.qti.hardware.cryptfshw@1.0 \
+    vendor.display.config@1.8 \
+
 # Build necessary packages for vendor
 PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service.pixel \
+    android.hardware.camera.provider@2.6-service-google \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.identity-support-lib.vendor \
+    chre \
+    ese_spi_st \
+    hardware.google.light@1.0.vendor:32 \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor \
+    libcppbor.vendor \
     libdisplayconfig \
     libhidltransport.vendor \
     libhwbinder.vendor \
     libjson \
-    libtinycompress.vendor \
+    libmedia_ecoservice.vendor \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_transport:64 \
+    libprotobuf-cpp-full-vendorcompat \
+    libqcomvoiceprocessingdescriptors \
+    libqti_vndfwk_detect.vendor \
+    librmnetctl \
+    libstagefright_bufferpool@2.0.1.vendor \
+    libteeui_hal_support.vendor \
+    libtinycompress \
     libtinyxml \
+    libvndfwk_detect_jni.qti.vendor \
     libwifi-hal-qcom \
+    memtrack.sm6150 \
+    nfc_nci.st21nfc.default \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    pixelatoms-cpp \
+    vendor.display.config@1.10.vendor \
+    vendor.display.config@1.11.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
+    vendor.qti.hardware.cryptfshw@1.0.vendor \
     vendor.qti.hardware.display.allocator@3.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
