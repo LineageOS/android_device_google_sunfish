4ee17f8713998c1a537ca848c418e7dd85650d97
Peter Cai <peter@typeblog.net>
2021-01-07 10:46:25 +0800

    sunfish: Bump vendor SPL and build fingerprint to 2021-01-05
    * Blobs have been re-extracted from RQ1A.210105.002
    
    Change-Id: I68cd4a5313465794427d81b4fdd17dd8e4304b2d

--- device-lineage.mk
+++ device-lineage.mk
@@ -104,4 +104,4 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapperextensions@1.1.vendor \
 
 # Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := "2020-12-05"
+VENDOR_SECURITY_PATCH := "2021-01-05"
