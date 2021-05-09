df0ff9a8da66261716c2b4b3a363a1588cc1f3d8
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    sunfish: Simplify AVB flag logic
    * AVB's `--flags 3` argument builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -48,7 +48,6 @@ BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-lineage/dynamic
 BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-lineage/vendor
 
 # Verified Boot
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 -include vendor/google/sunfish/BoardConfigVendor.mk
