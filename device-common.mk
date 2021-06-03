7aa896574f0abe58dc6106e28318e901e31ddbac
Lucas Dupin <dupin@google.com>
2021-06-03 16:45:21 -0700

    sunfish: Enable blurs
    Bug: 181092919
    Test: manual; performance scripts
    Change-Id: I5875a99167e80ddd5b79fc245f988d8a6ac167be

--- device-common.mk
+++ device-common.mk
@@ -91,6 +91,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_color_management=tr
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.has_wide_color_display=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.has_HDR_display=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.supports_background_blur=1
 
 # Must align with HAL types Dataspace
 # The data space of wide color gamut composition preference is Dataspace::DISPLAY_P3
