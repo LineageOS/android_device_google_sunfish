f97c1d67a6f37f0b5a6ea4a7ce878bc3fb363a53
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    sunfish: Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- device-lineage.mk
+++ device-lineage.mk
@@ -12,6 +12,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display \
     vendor/qcom/opensource/display
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
