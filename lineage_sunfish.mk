0c381bd746fcd2f883ce97d6df111a3cf063fae4
Michael Bestas <mkbestas@lineageos.org>
2022-02-10 22:58:11 +0200

    Update fingerprint/build description from SQ1A.220205.002
    Change-Id: I693c5bc8b922a38fce568b118331773f5448a917

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 12 SQ1A.220105.002 7961164 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 12 SQ1A.220205.002 8010174 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:12/SQ1A.220105.002/7961164:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:12/SQ1A.220205.002/8010174:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
