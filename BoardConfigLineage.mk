3ea68a57ded6ba46ffbeedeee243177f81c2d4f5
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    sunfish: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,7 +13,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/tools-lineage/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := sunfish_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
