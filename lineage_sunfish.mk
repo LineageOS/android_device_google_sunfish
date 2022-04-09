c8df8df7f02f5ff859499c4b449cc13da93a670c
Michael Bestas <mkbestas@lineageos.org>
2022-04-10 00:17:47 +0300

    Update fingerprint/build description from SP2A.220405.003
    Change-Id: I8709962d62e35a552ac063ff9c6ef97356d3dabd

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 12 SP2A.220305.012 8177914 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 12 SP2A.220405.003 8210211 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220305.012/8177914:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220405.003/8210211:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
