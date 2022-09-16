b2200ee74ac7135d848b7404b409f5a9c7c4250c
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-17 04:47:50 +0530

    Enable GRALLOC_HANDLE_HAS_RESERVED_SIZE
    * Need to make videos work in google camera on 13
    
    Change-Id: I0d78f44386129b1fad30bec6ef82000df130d631

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -17,6 +17,9 @@ TARGET_NEEDS_DTBOIMAGE := true
 DEVICE_MANIFEST_FILE += device/google/sunfish/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
+# Needed for camera
+TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
