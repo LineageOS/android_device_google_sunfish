64659117387198e4859d83d3cee89a7d7bc18169
Habib Moukalled <habibm@google.com>
2022-09-26 15:40:44 +0000

    Removing deprecated Sunfish target
    Notes:
    - This CL removes the deprecated Sunfish
    build target.
    
    Bug: 242059701
    Test: E2E enroll/auth, atest
    Change-Id: Ibb020066fd94ebc2089bd4da7cf00bc1c45b027c
    Merged-In: Ibb020066fd94ebc2089bd4da7cf00bc1c45b027c

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -252,13 +252,6 @@ BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 4873781248
 # Set error limit to BOARD_SUPER_PARTITON_SIZE - 500MB
 BOARD_SUPER_PARTITION_ERROR_LIMIT := 9231663104
 
-# Note as of b/216531063, our sunfish_tuscany builds are
-# exceeding the super partition limit, causing all builds
-# to fail. Here we increase it by 600 MB to keep building.
-ifeq (,$(filter-out sunfish_tuscany, $(TARGET_PRODUCT)))
-BOARD_SUPER_PARTITION_ERROR_LIMIT := 9831663104
-endif
-
 # DTB
 ifeq (,$(filter-out sunfish_kasan, $(TARGET_PRODUCT)))
 BOARD_PREBUILT_DTBIMAGE_DIR := device/google/sunfish-kernel/kasan
