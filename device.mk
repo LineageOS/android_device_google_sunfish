317e6a5d110de783e7e2b0845ec0c4d64c8bbd33
Steve Berbary <sberbary@google.com>
2023-02-10 02:53:29 +0000

    Update sunfish SVN to 58
    Bug: 268558932
    Change-Id: Ic6c13ee8be4d27502645633d30e075489d15f026

--- device.mk
+++ device.mk
@@ -886,7 +886,7 @@ PRODUCT_PACKAGES += $(HIDL_WRAPPER)
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.build.svn=57
+	ro.vendor.build.svn=58
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
