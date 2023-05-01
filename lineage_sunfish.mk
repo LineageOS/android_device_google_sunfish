7ed7ec670b34e35e44251845f9665a677273c1e3
Michael Bestas <mkbestas@lineageos.org>
2023-05-02 01:19:42 +0300

    Update fingerprint/build description from TQ2A.230505.002
    Change-Id: I22bb96f12fe56df4caf1cb575632fe91dfa91ce1

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 13 TQ2A.230405.003 9719927 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 13 TQ2A.230505.002 9891397 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ2A.230405.003/9719927:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ2A.230505.002/9891397:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
