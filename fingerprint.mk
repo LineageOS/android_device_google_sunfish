136ff69cbaf943397db65ee51d4b0c1762cf8a55
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 02:36:59 +0200

    sunfish: Cleanup dead targets
    Enable EPPE while we are at it.
    
    Change-Id: I5f94d5f1cd00a4bf2907b0063bd997abad2b39df

--- fingerprint.mk
+++ fingerprint.mk
@@ -1,7 +1,4 @@
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.2-service.fpc \
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
