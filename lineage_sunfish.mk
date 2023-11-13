81a385155bfb2dc8e11dc8b80712f930c6ed15a4
razorloves <razorloves@gmail.com>
2023-11-12 22:48:57 -0600

    Update fingerprint/build description from TQ3A.230805.001.S1
    Change-Id: I883bd5cb0e010f2d585563e388623f99db6732e7
    (cherry picked from commit 4803ed4cce50dbb4b54186109300ed9a5a5846dc)

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 13 TQ3A.230805.001 10316531 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 13 TQ3A.230805.001.S1 10786265 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ3A.230805.001/10316531:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ3A.230805.001.S1/10786265:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
