9a889cc666398b302b12749e1b6fc8cdfd33226f
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    sunfish: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device-lineage.mk
+++ device-lineage.mk
@@ -57,162 +57,31 @@ PRODUCT_PACKAGES += \
 # Touch
 include hardware/google/pixel/touch/device.mk
 
-# Build necessary packages for system_ext
-
-# Audio
-PRODUCT_PACKAGES += \
-    libaudioroute.vendor
-
-# Display
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
-    android.hardware.radio@1.6 \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.2 \
-    vendor.display.config@1.3 \
-    vendor.display.config@1.4 \
-    vendor.display.config@1.5 \
-    vendor.display.config@1.6 \
-    vendor.display.config@1.7 \
-    vendor.display.config@1.8
-
 # Build necessary packages for vendor
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor \
-    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
-    hardware.google.bluetooth.sar@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
-
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.device-V1-ndk.vendor:64 \
-    android.hardware.camera.provider-V1-ndk.vendor:64
 
 # CHRE
 PRODUCT_PACKAGES += \
     chre
 
-# Codec2
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0.vendor \
-    libavservices_minijail.vendor \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor \
-    libstagefright_bufferpool@2.0.1.vendor
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
-
-# Confirmation UI
-PRODUCT_PACKAGES += \
-    android.hardware.confirmationui@1.0.vendor:64 \
-    libteeui_hal_support.vendor:64
-
-# Display
-PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
-    vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
     flp.conf
 
-# HIDL
-PRODUCT_PACKAGES += \
-    libhwbinder.vendor
-
 # Identity credential
 PRODUCT_PACKAGES += \
-    android.hardware.identity-V4-ndk.vendor:64 \
-    android.hardware.identity-support-lib.vendor:64 \
     android.hardware.identity_credential.xml
-
-# Json
-PRODUCT_PACKAGES += \
-    libjson
-
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat
-
-# Sensor listener
-PRODUCT_PACKAGES += \
-    lib_sensor_listener
-
-# Tinycompress
-PRODUCT_PACKAGES += \
-    libtinycompress
 
 # VNDK FWK detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-V2-ndk.vendor:64 \
-    android.hardware.wifi@1.0.vendor:64 \
-    libwifi-hal:64 \
-    libwifi-hal-ctrl:64 \
-    libwifi-hal-qcom \
-    libwifi-system-iface.vendor:64
-
-# Misc interfaces
-PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor \
-    android.frameworks.stats@1.0.vendor:64 \
-    android.hardware.authsecret@1.0.vendor \
-    android.hardware.biometrics.fingerprint@2.1.vendor:64 \
-    android.hardware.biometrics.fingerprint@2.2.vendor:64 \
-    android.hardware.camera.common@1.0.vendor:64 \
-    android.hardware.camera.device@1.0.vendor:64 \
-    android.hardware.camera.device@3.2.vendor:64 \
-    android.hardware.camera.provider@2.4.vendor:64 \
-    android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.input.common-V1-ndk.vendor:64 \
-    android.hardware.input.processor-V1-ndk.vendor:64 \
-    android.hardware.keymaster-V3-ndk.vendor:64 \
-    android.hardware.keymaster@3.0.vendor \
-    android.hardware.keymaster@4.0.vendor \
-    android.hardware.keymaster@4.1.vendor \
-    android.hardware.neuralnetworks@1.0.vendor:64 \
-    android.hardware.neuralnetworks@1.1.vendor:64 \
-    android.hardware.neuralnetworks@1.2.vendor:64 \
-    android.hardware.neuralnetworks@1.3.vendor:64 \
-    android.hardware.oemlock@1.0.vendor:64 \
-    android.hardware.power-V1-ndk.vendor \
-    android.hardware.radio.config@1.0.vendor:64 \
-    android.hardware.radio.config@1.1.vendor:64 \
-    android.hardware.radio.config@1.2.vendor:64 \
-    android.hardware.radio.deprecated@1.0.vendor:64 \
-    android.hardware.radio@1.2.vendor:64 \
-    android.hardware.radio@1.3.vendor:64 \
-    android.hardware.radio@1.4.vendor:64 \
-    android.hardware.radio@1.5.vendor:64 \
-    android.hardware.rebootescrow-V1-ndk.vendor:64 \
-    android.hardware.secure_element@1.1.vendor:64 \
-    android.hardware.secure_element@1.2.vendor:64 \
-    android.hardware.sensors@1.0.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.hardware.sensors@2.0.vendor \
-    android.hardware.sensors@2.1.vendor \
-    android.hardware.thermal@2.0.vendor:64 \
-    android.hardware.weaver@1.0.vendor:64 \
-    android.system.net.netd@1.1.vendor:64
-
-# Misc
-PRODUCT_PACKAGES += \
-    libcrypto_utils.vendor:64 \
-    libpng.vendor \
-    libsqlite.vendor \
-    libssl.vendor:32
+    libwifi-hal-ctrl:64
 
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
