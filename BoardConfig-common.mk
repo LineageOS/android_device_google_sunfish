de51b7308d4830796aa77b297b8e38d2dba6efdd
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 23:34:09 +0300

    Revert "AOSP+GMS - Build for aosp_sunfish_gms - set BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE."
    Reason for revert: Doesn't affect our builds.
    
    This reverts commit 9d089197da70325478ce424b8e6cfb8db36a6fac.
    
    Change-Id: I820131115e49698840f6c30ddcdf3da2f760315d

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -35,9 +35,6 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
-# GOOGLE: workaround for https://android-review.googlesource.com/c/platform/system/sepolicy/+/1532995
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
-
 TARGET_BOARD_COMMON_PATH := device/google/sunfish/sm7150
 
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
