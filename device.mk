722436009a0d9dad96173af3ce8d0476dbd7cac2
Michael Bestas <mkbestas@lineageos.org>
2025-01-27 12:44:55 +0330

    sunfish: Migrate mount point creation out of AndroidBoard.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
     $(LOCAL_PATH)/init.ramoops.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.ramoops.sh \
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_firmware_mnt_mountpoint
+
 MSM_VIDC_TARGET_LIST := sm6150 # Get the color format from kernel headers
 MASTER_SIDE_CP_TARGET_LIST := sm6150 # ION specific settings
 
--- sunfish/AndroidBoard.mk
+++ sunfish/AndroidBoard.mk
@@ -1,14 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-#A/B builds require us to create the mount points at compile time.
-#Just creating it for all cases since it does not hurt.
-FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
-ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
-
-$(FIRMWARE_MOUNT_POINT):
-	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
-
 # copy kernel headers to the build tree
 $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
 	rm -rf $@
