28b195391d35009f7f81c23c13bdfdf05b86514a
Michael Bestas <mkbestas@lineageos.org>
2025-03-17 14:40:33 +0200

    fixup! Change TARGET_RECOVERY_UI_LIB to use fully qualified names in sunfish
    Change-Id: Ie0e77bd84e757593124a71ff34422ea682f595fb

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -84,7 +84,7 @@ TARGET_RECOVERY_WIPE := device/google/sunfish/recovery.wipe
 TARGET_RECOVERY_FSTAB := device/google/sunfish/fstab.hardware
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_LIB := \
-    //hardware/google/pixel/recovery:librecovery_ui_pixel
+    //hardware/google/pixel/recovery:librecovery_ui_pixel \
     libfstab
 
 # Enable chain partition for system.
