a824a45b5980faa5cf7800bf0f83a12f3108a433
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    sunfish: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,7 +13,6 @@ TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/sunfish/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
