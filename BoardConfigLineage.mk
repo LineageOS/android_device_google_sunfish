4ed41dd6218236bf1ee7b0356ea3ef7a46af3544
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    sunfish: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -12,7 +12,6 @@ TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/sunfish/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Partitions
