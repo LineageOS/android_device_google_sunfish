74fa5b69c327fb46cc1e0d59d4960c7280776a73
Anand S <anandzzz360@gmail.com>
2026-01-07 19:52:47 +0530

    sunfish: Enable Lineage Health Fast charge control
    Change-Id: Ibd9709afd3ee146a011dabe56abb10a42efe4240

--- device-lineage.mk
+++ device-lineage.mk
@@ -50,6 +50,9 @@ include hardware/google/pixel/lineage_health/device.mk
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/sm7150_bms/charge_disable)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/restrict_chg)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
